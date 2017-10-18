8884341366aba854bb91cdbd6c22c1208cd2af76
meng.yue <yebanhuakai@qq.com>
2017-10-18 16:37:03 +0800

    Initial Commit

---
+++ AFNetworkTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkTest
+//
+//  Created by Shenry on 2017/10/18.
+//  Copyright © 2017年 Shenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFNetworkTest
+//
+//  Created by Shenry on 2017/10/18.
+//  Copyright © 2017年 Shenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
